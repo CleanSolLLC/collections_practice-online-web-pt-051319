ee8570048f5c3ff2aafa154993493e7871c72dd8
Mark May <mamark90@gmail.com>
2019-05-23 18:01:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,3 +58,9 @@ def add_s(string_array)
    end
   end
  end
+
+ def swap_elements_from_to(array, index, destination_index)
+   array.collect {|name| name, name[destination_index], name[index]}
+ end
+
+   
