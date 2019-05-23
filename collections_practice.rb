1a561b4656fca78bcdfe64f56f7a85f3e408a589
Mark May <mamark90@gmail.com>
2019-05-23 16:18:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,6 +51,6 @@ end
 
 def add_s(string_array)
   new_array =[]
-  string_array.each_with_index.collect{|element, index| puts "#{element}" puts "#{index}"}
+  string_array.each_with_index.collect{|element, index| puts "#{element} #{index}"}
   binding.pry
 end
