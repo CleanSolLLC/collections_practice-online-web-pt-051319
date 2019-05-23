36732970a0ecda6dccb5048fc4e226d4c608ce25
Mark May <mamark90@gmail.com>
2019-05-23 14:02:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,7 +31,7 @@ end
 
 def find_a(string_array)
   chars_contain_a = []
-  string_array.to_s.start_with?("a")
+  string_array.to_s.start_with?("apple")
     binding.pry
   #string_array.any?("a")
 end
