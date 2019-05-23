c398f785c4ac6cdb08728085292a381f99205b6a
Mark May <mamark90@gmail.com>
2019-05-23 13:49:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |word| word.length }
+end
+
+def swap_elements(array)
+array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker(string_array)
+  new_array =[]
+  string_array.each do |name|
+  name[2] = "$"
+  new_array << name
+  end
+new_array
+end
+
+def find_a(string_array)
+  chars_contain_a = []
+  string_array.any?("a")
+binding.pry
+end
